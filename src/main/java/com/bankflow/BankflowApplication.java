d0eb214c224d80ea488cf5b5e6f29d15d7957041
saivardhan_j <jakkasaivardhan98@gmail.com>
2026-09-03 19:42:13 +0530

    project created

---
+++ src/main/java/com/bankflow/BankflowApplication.java
@@ -0,0 +1,13 @@
+package com.bankflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankflowApplication.class, args);
+	}
+
+}
